cc562f0a2b5ebbd9ddf23659e703c6ce90d51f39
sanchayana2007 <sanchayana2007@gmail.com>
2014-08-18 19:47:49 +0530

    added header

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //Main file 
+// 
+
+
+// mongoose setup
+require('./data/models/product');
+ 
+var express = require( 'express' );
+var routes  = require( './routes' );
+var http    = require( 'http' );
+var path    = require( 'path' );
+var app     = express();
+var engine  = require( 'ejs-locals' );
+
+
