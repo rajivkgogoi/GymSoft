06b9d0680bacfdca23d9de923cdc5e8e85840bc5
sanchayana2007 <sanchayana2007@gmail.com>
2014-09-09 11:09:24 +0530

    3.x to 4.x migration

--- app.js
+++ app.js
@@ -9,31 +9,37 @@ var http    = require( 'http' );
 var path    = require( 'path' );
 var app     = express();
 var engine  = require( 'ejs-locals' );
-
+var favicon = require('serve-favicon');
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+var cookieparser = require('cookie-parser')
+var methodOverride = require('method-override')
+var session = require('express-session')
+var errorHandler = require('errorhandler')
 
 //all environments
 app.set( 'port', process.env.PORT || 3001 );
 app.engine( 'ejs', engine );
 app.set( 'views', path.join( __dirname, 'views' ));
 app.set( 'view engine', 'ejs' );
-app.use( express.favicon());
-app.use( express.logger( 'dev' ));
-app.use( express.bodyParser());
-app.use( express.json());
-app.use( express.urlencoded());
-app.use( express.methodOverride());
+app.use( favicon('Desert.jpg'));
+app.use( logger( 'dev' ));
+app.use(bodyParser());
+//app.use( express.json());
+//app.use( express.urlencoded());
+app.use( methodOverride());
 	//Session maintainence stuffs
-		app.use(express.cookieParser('my secret string'));
-		app.use(express.session({
+app.use(cookieparser('my secret string'));
+		app.use(session({
 				secret: 'my secret string',
 				maxAge: 3600000
 		}));
-app.use( app.router );
+//app.use( app.router );
 app.use( express.static( path.join( __dirname, 'public' )));
  
 // development only
 if ( 'development' == app.get( 'env' )) {
-  app.use( express.errorHandler());
+  app.use( errorHandler());
 }
 
 // Routes are separeted and kept in a different route folder having the GET/POSTs
