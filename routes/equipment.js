532ce00e436021d82137a9418f3695aad1947a90
sanchayana2007 <sanchayana2007@gmail.com>
2014-09-16 19:28:17 +0530

    serach advanced

--- routes/equipment.js
+++ routes/equipment.js
@@ -13,14 +13,14 @@ module.exports = function(app)
 	  console.log("DEBUG:/POST/Equipment/save:startDate "+ req.body.startDate);
 	  var lpost
 	  
-
+	console.log("DEBUG:/POST/Equipment/save:vendorname "+ req.session.user);
 	  Vendors.findOne({ 'vendorname': req.session.user }, 'vendornumber', function (err, vendor) {
 	  Equipment.findOne({}, {}, { sort: { 'Equipmentsnumber' : -1 } }, function(err, post) {
 	  if(post){
 	  	lpost = post.Equipmentnumber;
 	  }
 	  console.log( "DEBUG:/Equipment/save: last old Equipment Number" + post  + lpost)
-	  console.log( "DEBUG:/Equipment/save: vendor.vendornumber " + vendor.vendornumber)
+	  console.log( "DEBUG:/Equipment/save: vendor.vendor number " + vendor.vendornumber)
 	  if(lpost)	
 	  {
 	  	lpost = lpost + 1;
@@ -69,16 +69,74 @@ module.exports = function(app)
 	});
 	
 	app.post('/equipment/findresult', function(req, res) {
-
+		var random = 11111;
 		console.log("DEBUG:/SERach Advance serach" +  req.body.Advancedsearch);
 		if(req.body.Equipmentname && (req.body.Advancedsearch != 1)){
 		 var regex = new RegExp(req.body.Equipmentname, "i"),
 		 query = { Equipmentname: regex };
 		}
+		//we are at advance search
+		else 
+		{
+			
+			if(req.body.EquipmentPrice) {
+				if(req.body.type == "Greater than")
+					random= random -1;
+				else if(req.body.type == "Less than")
+					random= random -10;
+				else
+					random= random -100;
+			}
+			if(req.body.manufacturer)
+			{
+					random= random -1000;
+			}
+			if(req.body.Purchasedate)
+			{	
+				    random= random -10000;
+			}
+		}
 		
+		console.log("DEBUG:/SERach Advance search Random" +  random);
+		switch(random)
+		{
+		case 11110:
+			query = { EquipmentPrice: { $gt : req.body.EquipmentPrice}};
+			break;
+		case 11101:
+			query = { EquipmentPrice: { $lt : req.body.EquipmentPrice}};
+			break;
+		case 11011:
+			query = { EquipmentPrice:  req.body.EquipmentPrice};
+			break;			
+		case 10111:
+			var regex = new RegExp(req.body.manufacturer, "i");
+			console.log("DEBUG:/SERach Advance search manufacturer" +  req.body.manufacturer);
+			query = { manufacturer: regex};	
+			break;
+		case 10110:
+			var regex = new RegExp(req.body.manufacturer, "i"),
+			query = { $and : [{  manufacturer: regex },{EquipmentPrice: { $gt : req.body.EquipmentPrice}}]};
+			break;
+		case 10101:
+			var regex = new RegExp(req.body.manufacturer, "i"),
+			query = { $and : [{  manufacturer: regex },{EquipmentPrice: { $lt : req.body.EquipmentPrice}}]};
+			break;
+		case 10011:
+			var regex = new RegExp(req.body.manufacturer, "i"),
+			query = { $and : [{  manufacturer: regex },{EquipmentPrice: req.body.EquipmentPrice}]};
+			break;
+		case 1111:
+			var regex = new RegExp(req.body.manufacturer, "i"),
+			query = { $and : [{  manufacturer: regex },{Purchasedate: req.body.Purchasedate}]};
+			break;
+			
+		}
 		
 		if(query){
-		Equipment.find(query).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20).exec( function ( err, equipments ){
+		console.log("DEBUG:/SERach Advance search QUERYr" +  require('util').inspect(query, {depth:null}));
+		Equipment.find(query).exec( function ( err, equipments ){
+		//Equipment.find(query).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20).exec( function ( err, equipments ){
 		console.log("DEBUG:/SERach results"+ require('util').inspect(equipments, {depth:null}) );
 		res.render('modules/equipsearchview', {title: "Search results" ,session: req.session,ListProduct:equipments});
 		});
